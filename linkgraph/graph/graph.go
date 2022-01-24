d77c64538e6152d51d5d04547cdf27487f17f16d
Waqas Shah <wshah052@uottawa.ca>
2022-01-24 14:41:20 +0500

    Added the followings
    - The graph interface.
    - Edge and Link structs

---
+++ linkgraph/graph/graph.go
@@ -0,0 +1,34 @@
+package graph
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Link struct {
+	ID				uuid.UUID
+	URL 			string
+	RetrievedAt 	time.Time
+}
+
+type Edge struct {
+	ID	uuid.UUID
+	Src	uuid.UUID
+	Dst uuid.UUID
+	updatedAt	time.Time
+
+}
+
+
+type Graph interface {
+	UpserLink(link *Link) error
+	FindLink(link *Link) (*Link, error)
+
+	UpsertEdge(edge *Edge) error
+	RemoveStaleEdges(fromID uuid.UUID, updatedBefore time.Time) error
+
+	Links(fromID, toID, uuid.UUID, retrievedBefore time.Time) (LinkIterator, error)
+	Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (EdgeIterator, error) 
+}
+

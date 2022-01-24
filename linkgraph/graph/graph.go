de955f69b3424be5385b223c80c11f2e105b4546
Waqas Shah <wshah052@uottawa.ca>
2022-01-24 14:56:18 +0500

    Removed bug in Graph interface

--- linkgraph/graph/graph.go
+++ linkgraph/graph/graph.go
@@ -7,19 +7,17 @@ import (
 )
 
 type Link struct {
-	ID				uuid.UUID
-	URL 			string
-	RetrievedAt 	time.Time
+	ID          uuid.UUID
+	URL         string
+	RetrievedAt time.Time
 }
 
 type Edge struct {
-	ID	uuid.UUID
-	Src	uuid.UUID
-	Dst uuid.UUID
-	updatedAt	time.Time
-
+	ID        uuid.UUID
+	Src       uuid.UUID
+	Dst       uuid.UUID
+	updatedAt time.Time
 }
-
 
 type Graph interface {
 	UpserLink(link *Link) error
@@ -28,7 +26,6 @@ type Graph interface {
 	UpsertEdge(edge *Edge) error
 	RemoveStaleEdges(fromID uuid.UUID, updatedBefore time.Time) error
 
-	Links(fromID, toID, uuid.UUID, retrievedBefore time.Time) (LinkIterator, error)
-	Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (EdgeIterator, error) 
+	Links(fromID, toID uuid.UUID, retrievedBefore time.Time) (LinkIterator, error)
+	Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (EdgeIterator, error)
 }
-

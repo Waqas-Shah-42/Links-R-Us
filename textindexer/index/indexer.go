179cc6a14e5f8bfe863c67286042365b1246c0e4
Waqas Shah <wshah052@uottawa.ca>
2022-02-07 14:05:44 +0500

    Added indexer.go

--- textindexer/index/indexer.go
+++ textindexer/index/indexer.go
@@ -1,3 +1,72 @@
 package indexer
 
+import (
+	"time"
 
+	"github.com/google/uuid"
+)
+
+
+type Indexer interface {
+	Index(doc *Document) error
+	FindByID (linkID uuid.UUID) (*Document, error)
+	Search (query Query) (Iterator, error)
+ }
+
+type Document struct {
+	LinkID uuid.UUID
+
+	URL string
+
+	Title string
+	Content string
+
+	IndexedAt time.Time
+	PageRank float64
+}
+
+type Query struct {
+	Type QueryType
+	Expression string
+	Offset uint64
+}
+
+type QueryType uint8
+
+const (
+	QueryTypeMatch QueryType = iota
+	QueryTypePhase
+)
+
+type Iterator interface {
+	// close iterator
+	Close() error
+
+	// Loads the next documnet matching search query.
+	// Return false if no more documnets left
+	Next() bool
+
+	// Returns last error encountered by iterator
+	Error() error
+
+	// Returns current document
+	Document() *Document
+
+	// Returns approximate number of search results
+	TotalCount() uint64
+}
+
+/*
+
+How the iterator can be used
+// 'docIt' is a search iterator
+for docIt.Next() {
+doc := docIt.Document()
+// Do something with doc...
+}
+
+if err := docIt.Error(); err != nil {
+// Handle error...
+}
+
+*/

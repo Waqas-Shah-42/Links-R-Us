dd101f45c250654f9865ab94e6e0b25fea85c055
Waqas Shah <wshah052@uottawa.ca>
2022-02-14 16:50:23 +0500

    Worked on bleveIterator

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ deps:
 		dep ensure;\
 	fi
 
-test: 
+test:
 	@echo "[go test] running tests and collecting coverage metrics"
 	@go test -v -tags all_tests -race -coverprofile=coverage.txt -covermode=atomic ./...
 
--- textindexer/store/iterator.go
+++ textindexer/store/iterator.go
@@ -1,10 +1,69 @@
 package memory
 
-import "github.com/blevesearch/bleve"
+import (
+	"github.com/Waqas-Shah-42/Links-R-Us/textindexer/index"
+	"github.com/blevesearch/bleve"
+)
 
 type bleveIterator struct {
 	idx       *InMemoryBleveIndexer
 	searchReq *bleve.SearchRequest
-	cumIdx    uint64
-	rsIdx     int
+
+	cumIdx uint64
+	rsIdx  int
+	rs     *bleve.SearchResult
+
+	latchedDoc *index.Document
+	lastErr    error
+}
+
+// Next loads next document matching query.
+// Returns false if no more documents found
+func (it *bleveIterator) Next() bool {
+	if it.lastErr != nil || it.rs == nil || it.cumIdx >= it.rs.Total {
+		return false
+	}
+	if it.rsIdx >= it.rs.Hits.Len() {
+		it.searchReq.From += it.searchReq.Size
+		if it.rs, it.lastErr = it.idx.idx.Search(it.searchReq); it.lastErr != nil {
+			return false
+		}
+		it.rsIdx = 0
+	}
+
+	nextID := it.rs.Hits[it.rsIdx].ID
+	if it.latchedDoc, it.lastErr = it.idx.findByID(nextID); it.lastErr != nil {
+		return false
+	}
+	it.cumIdx++
+	it.rsIdx++
+	return true
+}
+
+// Close the iterator and release any allocated resources.
+func (it *bleveIterator) Close() error {
+	it.idx = nil
+	it.searchReq = nil
+	if it.rs != nil {
+		it.cumIdx = it.rs.Total
+	}
+	return nil
+}
+
+// Error returns the last error encountered by the iterator.
+func (it *bleveIterator) Error() error {
+	return it.lastErr
+}
+
+// Document returns the current document from the result set.
+func (it *bleveIterator) Document() *index.Document {
+	return it.latchedDoc
+}
+
+// TotalCount returns the approximate number of search results.
+func (it *bleveIterator) TotalCount() uint64 {
+	if it.rs == nil {
+		return 0
+	}
+	return it.rs.Total
 }
